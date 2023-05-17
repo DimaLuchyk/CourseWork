afc43c49cb30fca2ed6bef1d221bace556357229
dmytro.luchyk <dimaluchik7@gmail.com>
2023-05-17 22:55:28 +0300

    packet header

--- Components/Server/inc/Shared.h
+++ Components/Server/inc/Shared.h
@@ -4,19 +4,56 @@
 #include <cstddef>
 #include <cstdint>
 
+#include <QString>
+#include <QDateTime>
+
 static const std::byte STARTOFPACKET{0xA7};
 
 enum PacketType
 {
+    //log in & log up
+    LOG_IN,
+    LOG_IN_SUCCESS,
+    LOG_IN_FAILURE,
 
+    LOG_UP,
+    LOG_UP_SUCCESS,
+    LOG_UP_FAILURE,
+
+    //base communication
+    GET_EXISTED_FILES,
+    GET_EXISTED_FILES_SUCCESS,
+    GET_EXISTED_FILES_FAILURE,
+
+    ADD_FILE,
+    ADD_FILE_SUCCESS,
+    ADD_FILE_FAILURE,
+
+    REMOVE_FILE,
+    REMOVE_FILE_SUCCESS,
+    REMOVE_FILE_FAILURE,
+
+    DOWNLOAD_FILE,
+    DOWNLOAD_FILE_SUCCESS,
+    DOWNLOAD_FILE_FAILURE
 };
 
 struct PacketHeader
 {
-    std::byte startofpacket;
-    PacketType packetType;
-    std::uint32_t payloadLength;
+    std::byte startofpacket;            //indicates the start of packet
+    std::uint16_t sequenceNumber;            //sequence number
+    PacketType packetType;              //packet type
+    QString timeStamp;                  //time stamp
+    std::uint32_t payloadLength;        //the length of the payload
     //payload
 };
+
+PacketHeader generatePacketHeader(const PacketType packetType, const std::uint32_t payloadLength)
+{
+    QDateTime timeStamp = QDateTime::currentDateTimeUtc();
+    QString timeStampString = timeStamp.toString(Qt::ISODate);
+
+    return {STARTOFPACKET, 0, packetType, timeStampString, payloadLength};
+}
 
 #endif //SERVER_SHARED_H
